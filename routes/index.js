7ca7a751a66ff0e5e90eceebb685930cb6d2e1a1
KU Lok Sun <sundaykuloksun@gmail.com>
2017-11-29 18:30:12 +0800

    Implement mock live server

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 var fs = require('fs');
 var rawM3u8;
 var parsedM3u8;
-fs.readFile('../public/1280x720/BigBuckBunny_1280x720.m3u8', 'utf8', function(err, rawM3u8) {
+fs.readFile(path.join(__dirname, '../public/1280x720/BigBuckBunny_1280x720.m3u8'), 'utf8', function(err, rawM3u8) {
     var m3u8Parser = require('m3u8-parser');
     var parser = new m3u8Parser.Parser();
 
@@ -20,14 +20,21 @@ fs.readFile('../public/1280x720/BigBuckBunny_1280x720.m3u8', 'utf8', function(er
     parsedM3u8 = parser.manifest;
     setInterval(
         function() {
-            seq = `#EXTM3U
+            console.log('updated');
+            liveM3u8 = `#EXTM3U
 #EXT-X-VERSION:3
 #EXT-X-TARGETDURATION:18
-#EXT-X-MEDIA-SEQUENCE:${seq}`;
-            for (var i = seq; i < seq + 5; i++) {
-                parsedM3u8.segments[i];
-                seq += '';
+#EXT-X-MEDIA-SEQUENCE:${seq}
+`;
+            console.log(parsedM3u8);
+            for (var i = seq; i < seq + 3; i++) {
+                var ts = parsedM3u8.segments[i];
+                console.log(ts);
+                liveM3u8 += `#EXTINF:${ts.duration},
+${ts.uri}
+`;
             }
+            seq += 1;
         },
         10000
     );
@@ -42,23 +49,40 @@ router.get('/video.m3u8', function(req, res, next) {
 #EXT-X-VERSION:3
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2962000,NAME="High",CODECS="avc1.66.30",RESOLUTION=1280x720
 BigBuckBunny_1280x720.m3u8`;
+  res.header('Access-Control-Allow-Origin', 'http://dev.hk01.com');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.send(data);
 });
 
 router.get('/live.m3u8', function(req, res, next) {
+    console.log(req.header);
   data = `#EXTM3U
 #EXT-X-VERSION:3
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2962000,NAME="High",CODECS="avc1.66.30",RESOLUTION=1280x720
 chunklist_live.m3u8`;
+  res.header('Access-Control-Allow-Origin', 'http://dev.hk01.com');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.send(data);
 });
 
 router.get('/chunklist_live.m3u8', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://dev.hk01.com');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.send(liveM3u8);
 });
 
 router.get(/(.+)/, function(req, res, next) {
   var filepath = path.join(__dirname, '../public/1280x720/' + req.params[0]);
+  res.header('Access-Control-Allow-Origin', 'http://dev.hk01.com');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.sendFile(filepath);
 });
 
